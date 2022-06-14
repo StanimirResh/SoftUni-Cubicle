729a68d71c04421ec51328df3253546818515517
StanimirResh <mchorko@gmail.com>
2022-06-14 16:30:42 +0300

    404 errors added

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const aboutController = require('./controllers/aboutController');
 const createController = require('./controllers/createController');
+const notFoundController = require('./controllers/notFoundController');
 
 router.get('/', homeController)
 router.get('/about', aboutController);
 router.get('/create', createController);
+router.all('*', notFoundController);
 
 module.exports = router;
