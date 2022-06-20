3bcb096cd810ad405f73f31e2e78d74b338b3af8
StanimirResh <mchorko@gmail.com>
2022-06-21 01:18:30 +0300

    added config file

--- src/index.js
+++ src/index.js
@@ -7,17 +7,13 @@ const router = require('./routes.js');
 const mongoose = require('mongoose');
 
 const app = express();
-
 app.use('/static', express.static('public'))
 app.use(express.urlencoded({
     extended: false
 }))
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs'
-}));
 
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
+require('./config/handlebars.js')(app);
+
 
 app.use(router)
 
